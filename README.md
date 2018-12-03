4371c7e8123f7b989cf30de4f8abaca6e923ec1a
xXxRisingTidexXx <danylopetrik@gmail.com>
2018-12-03 23:32:58 +0200

    Game Session class has been added; score widget has been implemented as well

--- README.md
+++ README.md
@@ -5,8 +5,9 @@ Use left and right arrows to move your tool. Score, compete and survive!
 # v0.0.2
 - Main menu has been fully implemented;
 - Info menu has been implemented;
-- RM (ResourceManager) and WM (WidgetManager) classes has been added;
+- ResourceManager (ResourceManager) and WidgetManager (WidgetManager) classes has been added;
 - Game class has been partially implemented.
+
 Now PingPong seems to look like a very simple game. It has chained game loop and more or less stable menus. Soon I'll
 add Summary class to estimate player's statistics and results  in InfoMenu table. Also I gonna extend and embellish
 UI via adding score counter, extra game objects and background decorations.
--- pingpong.py
+++ pingpong.py
@@ -3,20 +3,23 @@ from time import sleep
 from tkinter import *
 from tkinter.ttk import Separator
 from yaml import load, dump
+from datetime import datetime
 
 
-# Maybe it'll be better to create all-game cache for different temporary data
 class App:
     DATA_PATH = 'res/app.yaml'
+    RESOURCE_MANAGER = None
+    WIDGET_MANAGER = None
+    CACHE = None
 
     def __init__(self):
-        self.rm = RM()
-        self.wm = WM(self.rm)
-        self.cache = Cache(self.rm)
-        data = self.rm[App]
-        self.tk = self.wm.prepare_tk(data['tk'])
+        App.RESOURCE_MANAGER = ResourceManager()
+        App.WIDGET_MANAGER = WidgetManager(App.RESOURCE_MANAGER)
+        App.CACHE = Cache(App.RESOURCE_MANAGER)
+        data = App.RESOURCE_MANAGER[App]
+        self.tk = App.WIDGET_MANAGER.prepare_tk(data['tk'])
         self.delay = data['delay']
-        self.main_menu = MainMenu(self.rm, self.wm, self.cache, self.tk)
+        self.main_menu = MainMenu(self.tk)
 
     def start(self):
         while self.main_menu.active:
@@ -25,16 +28,16 @@ class App:
             sleep(self.delay)
 
 
-class RM:
+class ResourceManager:
     FONTS = 'fonts'
     STYLES = 'styles'
     POSITIONS = 'positions'
 
     def __init__(self):
         self.data = {
-            RM.FONTS: self.read('res/fonts.yaml'),
-            RM.STYLES: self.read('res/styles.yaml'),
-            RM.POSITIONS: self.read('res/positions.yaml'),
+            ResourceManager.FONTS: self.read('res/fonts.yaml'),
+            ResourceManager.STYLES: self.read('res/styles.yaml'),
+            ResourceManager.POSITIONS: self.read('res/positions.yaml'),
             App: self.read(App.DATA_PATH),
             MainMenu: self.read(MainMenu.DATA_PATH),
             Game: self.read(Game.DATA_PATH),
@@ -57,9 +60,9 @@ class RM:
             dump(data, stream, default_flow_style=False)
 
 
-class WM:
-    def __init__(self, rm):
-        self.rm = rm
+class WidgetManager:
+    def __init__(self, resource_manager):
+        self.resource_manager = resource_manager
 
     # noinspection PyMethodMayBeStatic
     def prepare_tk(self, data):
@@ -72,43 +75,45 @@ class WM:
         return tk
 
     def prepare_frame(self, master, data):
-        frame = Frame(master, self.rm[RM.STYLES][data['style']])
-        frame.place(self.rm[RM.POSITIONS][data['position']])
+        frame = Frame(master, self.resource_manager[ResourceManager.STYLES][data['style']])
+        frame.place(self.resource_manager[ResourceManager.POSITIONS][data['position']])
         frame.update()
         return frame
 
     def prepare_button(self, master, data, command):
-        button = Button(master, self.rm[RM.STYLES][data['style']])
-        button.configure(text=data['text'], font=self.rm[RM.FONTS][data['font']], command=command)
-        button.pack(self.rm[RM.POSITIONS][data['position']])
+        button = Button(master, self.resource_manager[ResourceManager.STYLES][data['style']])
+        button.configure(
+            text=data['text'], font=self.resource_manager[ResourceManager.FONTS][data['font']], command=command
+        )
+        button.pack(self.resource_manager[ResourceManager.POSITIONS][data['position']])
         button.update()
         return button
 
     # noinspection PyMethodMayBeStatic
-    def prepare_canvas(self, master, data, width, height):
-        canvas = Canvas(master, width=width, height=height)
-        canvas.configure(data)
-        canvas.pack()
+    def prepare_canvas(self, master, data):
+        canvas = Canvas(master, width=master.winfo_width(), height=master.winfo_height())
+        canvas.configure(self.resource_manager[ResourceManager.STYLES][data['style']])
+        canvas.pack(self.resource_manager[ResourceManager.POSITIONS][data['position']])
         canvas.update()
         return canvas
 
     def prepare_label(self, master, data):
-        label = Label(master, self.rm[RM.STYLES][data['style']])
-        label.configure(text=data['text'], font=self.rm[RM.FONTS][data['font']])
-        label.pack(self.rm[RM.POSITIONS][data['position']])
+        label = Label(master, self.resource_manager[ResourceManager.STYLES][data['style']])
+        label.configure(text=data['text'], font=self.resource_manager[ResourceManager.FONTS][data['font']])
+        label.pack(self.resource_manager[ResourceManager.POSITIONS][data['position']])
         label.update()
         return label
 
     def prepare_separator(self, master, data):
         separator = Separator(master, orient=data['orient'])
-        separator.pack(self.rm[RM.POSITIONS][data['position']])
+        separator.pack(self.resource_manager[ResourceManager.POSITIONS][data['position']])
         return separator
 
 
 class Cache:
-    def __init__(self, rm):
-        self.rm = rm
-        self.data = {Table.CONTENT: self.rm.read(Table.CONTENT_PATH)}
+    def __init__(self, resource_manager):
+        self.resource_manager = resource_manager
+        self.data = {Table.CONTENT: self.resource_manager.read(Table.CONTENT_PATH)}
 
     def __getitem__(self, key):
         return self.data[key]
@@ -120,13 +125,11 @@ class Cache:
         self[key] = updater(self[key], value)
 
     def save(self, key, path):
-        self.rm.write(self[key], path)
+        self.resource_manager.write(self[key], path)
 
 
 class Menu:
-    def __init__(self, rm, wm, tk):
-        self.rm = rm
-        self.wm = wm
+    def __init__(self, tk):
         self.tk = tk
         self.hidden = False
         self.frame = None
@@ -147,12 +150,11 @@ class Menu:
 class MainMenu(Menu):
     DATA_PATH = 'res/main_menu.yaml'
 
-    def __init__(self, rm, wm, cache, tk):
-        super().__init__(rm, wm, tk)
-        self.cache = cache
+    def __init__(self, tk):
+        super().__init__(tk)
         self.active = True
-        data = self.rm[MainMenu]
-        self.frame = self.wm.prepare_frame(self.tk, data['frame'])
+        data = App.RESOURCE_MANAGER[MainMenu]
+        self.frame = App.WIDGET_MANAGER.prepare_frame(self.tk, data['frame'])
         self.buttons = self.prepare_buttons(data['buttons'])
         self.last_player = {'name': 'Danya', 'result': 5}  # later we will delete it
         self.info_menu = None
@@ -160,16 +162,16 @@ class MainMenu(Menu):
 
     def prepare_buttons(self, data):
         return (
-            self.wm.prepare_button(self.frame, data['play_button'], self.__play),
-            self.wm.prepare_button(self.frame, data['info_button'], self.__info),
-            self.wm.prepare_button(self.frame, data['help_button'], self.__help),
-            self.wm.prepare_button(self.frame, data['exit_button'], self.__exit)
+            App.WIDGET_MANAGER.prepare_button(self.frame, data['play_button'], self.__play),
+            App.WIDGET_MANAGER.prepare_button(self.frame, data['info_button'], self.__info),
+            App.WIDGET_MANAGER.prepare_button(self.frame, data['help_button'], self.__help),
+            App.WIDGET_MANAGER.prepare_button(self.frame, data['exit_button'], self.__exit)
         )
 
     def __play(self):
         self.hide()
         Game(self).play()  # later we will extract game statistics from cache, and self.last_player will be deleted
-        self.cache.update(Table.CONTENT, self.last_player, self.__appender)
+        App.CACHE.update(Table.CONTENT, self.last_player, self.__appender)
 
     # noinspection PyMethodMayBeStatic
     def __appender(self, lst, value):
@@ -191,7 +193,7 @@ class MainMenu(Menu):
         self.help_menu.visualize()
 
     def __exit(self):
-        self.cache.save(Table.CONTENT, Table.CONTENT_PATH)
+        App.CACHE.save(Table.CONTENT, Table.CONTENT_PATH)
         self.active = False
 
 
@@ -199,16 +201,30 @@ class Game:
     DATA_PATH = 'res/game.yaml'
 
     def __init__(self, main_menu):
-        self.rm = main_menu.rm
-        self.wm = main_menu.wm
-        self.tk = main_menu.tk
         self.main_menu = main_menu
-        data = self.rm[Game]
+        self.tk = self.main_menu.tk
+        data = App.RESOURCE_MANAGER[Game]
+        self.canvas = App.WIDGET_MANAGER.prepare_canvas(self.tk, data['canvas'])
+        self.session = Session()
+        self.paddle = self.prepare_paddle(data['paddle'])
+        self.ball = self.prepare_ball(data['ball'])
+        self.score = self.prepare_score(data['score'])
         self.delay = data['delay']
-        self.canvas = self.wm.prepare_canvas(self.tk, data['canvas'], self.tk.winfo_width(), self.tk.winfo_height())
-        # self.score_label = self.wm.prepare_label(self.canvas, data['score_label'])
-        self.paddle = Paddle(self.canvas, data['paddle'])
-        self.ball = Ball(self.canvas, data['ball'], self.paddle.id, self.delay)
+
+    def prepare_paddle(self, data):
+        paddle = Paddle(self.canvas, data)
+        self.session[Paddle] = {'id': paddle.id}
+        return paddle
+
+    def prepare_ball(self, data):
+        ball = Ball(self.canvas, data, self.session)
+        self.session[Ball] = {'id': ball.id}
+        return ball
+
+    def prepare_score(self, data):
+        score = Score(self.canvas, data)
+        self.session[Score] = {'id': score.id}
+        return score
 
     def play(self):
         while self.ball.flies():
@@ -218,7 +234,19 @@ class Game:
             sleep(self.delay)
         self.canvas.pack_forget()
         self.main_menu.visualize()  # delete it later
-        # Summary(self.rm, self.tk, self.main_menu, ...)  put statistics here
+        # Summary(self.resource_manager, self.tk, self.main_menu, ...)  put statistics here
+
+
+class Session:
+    def __init__(self):
+        self.data = {}
+        self.start = datetime.now()
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
 
 
 class Paddle:
@@ -231,7 +259,9 @@ class Paddle:
         self.canvas.bind_all(data['right_arrow'], self.__move_right)
 
     def prepare_rectangle(self, data):
-        _id = self.canvas.create_rectangle(data['x1'], data['y1'], data['x2'], data['y2'], data['style'])
+        _id = self.canvas.create_rectangle(
+            data['x1'], data['y1'], data['x2'], data['y2'], App.RESOURCE_MANAGER[ResourceManager.STYLES][data['style']]
+        )
         self.canvas.move(_id, data['x0'], data['y0'])
         return _id
 
@@ -251,16 +281,18 @@ class Paddle:
 
 
 class Ball:
-    def __init__(self, canvas, data, paddle_id, dt):
+    def __init__(self, canvas, data, session):
         self.canvas = canvas
         self.id = self.prepare_oval(data['oval'])
-        self.paddle_id = paddle_id
+        self.paddle_id = session[Paddle]['id']
         self.dx = choice(data['dx'])
         self.dy = choice(data['dy'])
-        self.dt = dt
+        self.dt = data['dt']
 
     def prepare_oval(self, data):
-        _id = self.canvas.create_oval(data['x1'], data['y1'], data['x2'], data['y2'], data['style'])
+        _id = self.canvas.create_oval(
+            data['x1'], data['y1'], data['x2'], data['y2'], App.RESOURCE_MANAGER[ResourceManager.STYLES][data['style']]
+        )
         self.canvas.move(_id, data['x0'], data['y0'])
         return _id
 
@@ -302,14 +334,32 @@ class Ball:
     def touch_paddle(self, coords, paddle_coords):
         return paddle_coords[1] <= coords[3] <= paddle_coords[3]
 
+
+class Score:
+    def __init__(self, canvas, data):
+        self.canvas = canvas
+        self.id = self.prepare_text(data)
+
+    def prepare_text(self, data):
+        _id = self.canvas.create_text(
+            data['x1'], data['y1'], text=data['text'], font=App.RESOURCE_MANAGER[ResourceManager.FONTS][data['font']]
+        )
+        self.canvas.itemconfigure(_id, App.RESOURCE_MANAGER[ResourceManager.STYLES][data['style']])
+        return _id
+
+    def increment(self):
+        value = self.canvas.itemcget(self.id, 'text')
+        self.canvas.itemconfigure(self.id, text=str(value + 1))
+
+
 # class Summary:
 #     DATA_PATH = 'res/summary.yaml'
 #
-#     def __init__(self, rm, tk, main_menu, statistics):
-#         self.rm = rm
+#     def __init__(self, resource_manager, tk, main_menu, statistics):
+#         self.resource_manager = resource_manager
 #         self.tk = tk
 #         self.main_menu = main_menu
-#         data = self.rm[Summary]
+#         data = self.resource_manager[Summary]
 #         self.frame = self.main_menu.prepare_frame(...)
 #         pass
 #
@@ -324,12 +374,12 @@ class InfoMenu(Menu):
     DATA_PATH = 'res/info_menu.yaml'
 
     def __init__(self, main_menu):
-        super().__init__(main_menu.rm, main_menu.wm, main_menu.tk)
-        data = self.rm[InfoMenu]
-        self.frame = self.wm.prepare_frame(self.tk, data['frame'])
-        self.table = Table(self, main_menu.cache, data['table'])
-        self.back_button = self.wm.prepare_button(self.frame, data['back_button'], self.__back)
+        super().__init__(main_menu.tk)
         self.main_menu = main_menu
+        data = App.RESOURCE_MANAGER[InfoMenu]
+        self.frame = App.WIDGET_MANAGER.prepare_frame(self.tk, data['frame'])
+        self.table = Table(self, data['table'])
+        self.back_button = App.WIDGET_MANAGER.prepare_button(self.frame, data['back_button'], self.__back)
 
     def __back(self):
         self.hide()
@@ -343,26 +393,25 @@ class Table:
     CONTENT = 'content'
     CONTENT_PATH = 'res/table.yaml'
 
-    def __init__(self, info_menu, cache, data):
+    def __init__(self, info_menu, data):
         self.info_menu = info_menu
-        self.cache = cache
         self.rows = data['rows']
-        self.header_label = self.info_menu.wm.prepare_label(self.info_menu.frame, data['header_label'])
+        self.header_label = App.WIDGET_MANAGER.prepare_label(self.info_menu.frame, data['header_label'])
         self.carcass = self.prepare_carcass(data['separator'], data['name_label'], data['result_label'])
         self.update()
 
     # noinspection PyUnusedLocal
     def prepare_carcass(self, separator_data, name_label_data, result_label_data):
         return [{
-            'separator': self.info_menu.wm.prepare_separator(self.info_menu.frame, separator_data),
-            'name_label': self.info_menu.wm.prepare_label(self.info_menu.frame, name_label_data),
-            'result_label': self.info_menu.wm.prepare_label(self.info_menu.frame, result_label_data)
+            'separator': App.WIDGET_MANAGER.prepare_separator(self.info_menu.frame, separator_data),
+            'name_label': App.WIDGET_MANAGER.prepare_label(self.info_menu.frame, name_label_data),
+            'result_label': App.WIDGET_MANAGER.prepare_label(self.info_menu.frame, result_label_data)
         } for i in range(self.rows)]
 
     def update(self):
-        content = self.limit(self.cache[Table.CONTENT])
+        content = self.limit(App.CACHE[Table.CONTENT])
         self.fill(content)
-        self.cache[Table.CONTENT] = content
+        App.CACHE[Table.CONTENT] = content
 
     def fill(self, content):
         for i in range(self.rows):
@@ -377,13 +426,13 @@ class HelpMenu(Menu):
     DATA_PATH = 'res/help_menu.yaml'
 
     def __init__(self, main_menu):
-        super().__init__(main_menu.rm, main_menu.wm, main_menu.tk)
-        data = self.rm[HelpMenu]
-        self.frame = self.wm.prepare_frame(self.tk, data['frame'])
-        self.header_label = self.wm.prepare_label(self.frame, data['header_label'])
-        self.wrapper_label = self.wm.prepare_label(self.frame, data['wrapper_label'])
-        self.back_button = self.wm.prepare_button(self.frame, data['back_button'], self.__back)
+        super().__init__(main_menu.tk)
         self.main_menu = main_menu
+        data = App.RESOURCE_MANAGER[HelpMenu]
+        self.frame = App.WIDGET_MANAGER.prepare_frame(self.tk, data['frame'])
+        self.header_label = App.WIDGET_MANAGER.prepare_label(self.frame, data['header_label'])
+        self.wrapper_label = App.WIDGET_MANAGER.prepare_label(self.frame, data['wrapper_label'])
+        self.back_button = App.WIDGET_MANAGER.prepare_button(self.frame, data['back_button'], self.__back)
 
     def __back(self):
         self.hide()
