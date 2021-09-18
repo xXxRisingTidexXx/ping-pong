d577471d54bd764d69d7677a74775462d5af21f1
d.petrakivskyi <d.petrakivskyi@smartweb.com.ua>
2021-09-18 17:34:16 +0300

    Defined application entrypoint

--- pingpong/__main__.py
+++ pingpong/__main__.py
@@ -1,4 +1,6 @@
 from tkinter import Tk
+from pingpong.theme import Color
+from pingpong.scenes import make_main_menu
 
 
 def main():
@@ -6,9 +8,9 @@ def main():
     tk.wm_title('ping-pong')
     tk.wm_geometry(f'{tk.winfo_screenwidth()}x{tk.winfo_screenheight()}')
     tk.wm_resizable(0, 0)
-    tk.wm_attributes('-topmost', 1, '-type', 'splash')
-    tk.configure(background='#000000')
-
+    # tk.wm_attributes('-topmost', 1, '-type', 'splash')
+    tk.configure(background=Color.background)
+    make_main_menu(tk)
     try:
         tk.mainloop()
     except KeyboardInterrupt:
