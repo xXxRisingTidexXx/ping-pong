43b9b27409cc76c6ee8b3ac82414c28915cd3dda
d.petrakivskyi <d.petrakivskyi@smartweb.com.ua>
2021-09-18 17:33:24 +0300

    Defined popular colors and fonts

---
+++ pingpong/theme.py
@@ -0,0 +1,11 @@
+class Color:
+    background = '#000000'
+    accent = '#c868db'
+    text = '#000000'
+    hover = '#ffffff'
+
+
+class Font:
+    button = 'Times 18 bold'
+    header = 'Courier 22 bold'
+    description = 'Verdana 14 normal'
