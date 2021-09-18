1b3cf5a6735a7bc4c967812ba3c65e35854aea0e
d.petrakivskyi <d.petrakivskyi@smartweb.com.ua>
2021-09-18 12:52:04 +0300

    Defined game entrypoint

--- pingpong/__main__.py
+++ pingpong/__main__.py
@@ -1,5 +1,18 @@
+from tkinter import Tk
+
+
 def main():
-    pass
+    tk = Tk()
+    tk.wm_title('ping-pong')
+    tk.wm_geometry(f'{tk.winfo_screenwidth()}x{tk.winfo_screenheight()}')
+    tk.wm_resizable(0, 0)
+    tk.wm_attributes('-topmost', 1, '-type', 'splash')
+    tk.configure(background='#000000')
+
+    try:
+        tk.mainloop()
+    except KeyboardInterrupt:
+        pass
 
 
 if __name__ == '__main__':
