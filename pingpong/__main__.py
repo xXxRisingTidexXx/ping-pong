13e27ecaa09d3556c5bd320ee4de4f19aac9370b
d.petrakivskyi <d.petrakivskyi@smartweb.com.ua>
2021-09-19 10:59:18 +0300

    Shortened main component name

--- pingpong/__main__.py
+++ pingpong/__main__.py
@@ -1,6 +1,6 @@
 from tkinter import Tk
 from pingpong.theme import Color
-from pingpong.scenes import make_main_menu
+from pingpong.scenes import make_menu
 
 
 def main():
@@ -10,7 +10,7 @@ def main():
     tk.wm_resizable(0, 0)
     # tk.wm_attributes('-topmost', 1, '-type', 'splash')
     tk.configure(background=Color.background)
-    make_main_menu(tk)
+    make_menu(tk)
     try:
         tk.mainloop()
     except KeyboardInterrupt:
