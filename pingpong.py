eee17fe213f409aa017ae708aada2ea4076743ab
d.petrakivskyi <d.petrakivskyi@smartweb.com.ua>
2021-09-26 13:38:07 +0300

    This is fine

--- pingpong.py
+++ pingpong.py
@@ -1,6 +1,6 @@
 from tkinter import Tk, Frame, Button, Label, Canvas, Event
 from typing import Callable
-from random import uniform
+from random import uniform, choice
 from pathlib import Path
 from pygame.mixer import init, music
 
@@ -86,8 +86,8 @@ def make_game(tk: Tk):
     canvas.setvar(GAME_IS_RUNNING)
     canvas.setvar(BALL_IS_MOVING)
     canvas.update()
-    paddle_width, paddle_height = 200, 15
-    paddle_x, paddle_y = (canvas_width - paddle_width) // 2, canvas_height - 300
+    paddle_width, paddle_height = 150, 10
+    paddle_x, paddle_y = (canvas.winfo_width() - paddle_width) // 2, canvas.winfo_height() - 300
     paddle_id = canvas.create_rectangle(
         paddle_x,
         paddle_y,
@@ -96,10 +96,10 @@ def make_game(tk: Tk):
         width=0,
         fill=SPRITE_COLOR
     )
-    paddle_vx = 10
+    paddle_vx = 8
     canvas.bind_all(KEY_LEFT, move_paddle_left(canvas, paddle_id, paddle_vx))
     canvas.bind_all(KEY_RIGHT, move_paddle_right(canvas, paddle_id, paddle_vx))
-    ball_x, ball_y, ball_r = canvas_width - 200, 100, 15
+    ball_x, ball_y, ball_r = 455, 300, 15
     ball_id = canvas.create_oval(
         ball_x,
         ball_y,
@@ -108,17 +108,13 @@ def make_game(tk: Tk):
         width=0,
         fill=SPRITE_COLOR
     )
+    ball_vx, ball_vy = uniform(-3, -1), uniform(-4, -2)
     delay = 10
-    # canvas.after(
-    #     0,
-    #     move_ball(canvas, ball_id, paddle_id, uniform(-2.5, -1), uniform(-0.1, 0.1), delay)
-    # )
     canvas.after(
         0,
-        move_ball(canvas, ball_id, paddle_id, uniform(0.4, 0.6), uniform(-0.9, -0.6), delay)
+        move_ball(canvas, ball_id, paddle_id, choice([ball_vx, -ball_vx]), ball_vy, delay)
     )
     canvas.after(0, check_fall(tk, canvas, ball_id, delay))
-    canvas.create_text(50, 70, text='0', font=SCORE_FONT, fill=ACCENT_COLOR)
 
 
 Handler = Callable[[Event], None]
@@ -149,41 +145,29 @@ def move_ball(
     paddle_id: int,
     vx: float,
     vy: float,
-    t: int
+    delay: int
 ) -> Callback:
     def move():
         ball_coords = canvas.coords(ball_id)
-        g = 0.002
-        dx, dy = vx * t, vy * t + g * t * t / 2
-        vx1, vy1 = vx, vy + g * t
-        if vx > 0:
-            xb, yb = ball_coords[2], ball_coords[3]
-            xr = canvas.winfo_width()
-            if x1 <= x2 <= x1 + dx:
-                dx = x2 - x1
-                dt = dx / vx
-                dy = vy * dt + g * dt * dt / 2
-                vx1 = -vx
-                vy1 = vy + g * dt
-        # vx1 = (
-        #     -vx * uniform(0.9, 1.1)
-        #     if ball_coords[0] <= 0 or ball_coords[2] >= canvas.winfo_width()
-        #     else vx
-        # )
-        # paddle_coords = canvas.coords(paddle_id)
-        # vy1 = t * 0.003 + (
-        #     -vy * uniform(0.9, 1.3)
-        #     if (
-        #         ball_coords[1] <= 0 or
-        #         ball_coords[2] >= paddle_coords[0] and
-        #         ball_coords[0] <= paddle_coords[2] and
-        #         paddle_coords[1] <= ball_coords[3] <= paddle_coords[3]
-        #     )
-        #     else vy
-        # )
-        canvas.move(ball_id, dx, dy)
+        vx1 = (
+            -vx * (1 + uniform(-0.1, 0.1))
+            if ball_coords[0] <= 0 or ball_coords[2] >= canvas.winfo_width()
+            else vx
+        )
+        paddle_coords = canvas.coords(paddle_id)
+        vy1 = delay * 0.002 + (
+            -vy
+            if (
+                ball_coords[1] <= 0 or
+                ball_coords[2] >= paddle_coords[0] and
+                ball_coords[0] <= paddle_coords[2] and
+                paddle_coords[1] <= ball_coords[3] <= paddle_coords[3]
+            )
+            else vy
+        )
+        canvas.move(ball_id, vx1, vy1)
         if canvas.getvar(GAME_IS_RUNNING) == '1':
-            canvas.after(t, move_ball(canvas, ball_id, paddle_id, vx1, vy1, t))
+            canvas.after(delay, move_ball(canvas, ball_id, paddle_id, vx1, vy1, delay))
         else:
             canvas.setvar(BALL_IS_MOVING, '0')
     return move
